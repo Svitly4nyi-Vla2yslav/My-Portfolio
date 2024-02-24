6b0f1f74eb9d980f79e89cdac65a9500bfa443ef
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-02-24 22:05:33 +0100

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/My-Portfolio/',
-});
-	
-	
+	plugins: [react()],
+	optimizeDeps: {
+		exclude: ["js-big-decimal"],
+	},
+      base: '/My-Portfolio/',
+    });
