4a78c483ed86bfdb1161c1e6ddadac974d93cd9d
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-02-25 20:56:11 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
-	optimizeDeps: {
-		exclude: ["js-big-decimal"],
-	},
-      base: '/',
-    });
+	base: '/',
+});
