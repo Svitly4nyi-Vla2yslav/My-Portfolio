054048d0c730a81e0fbe99f48ff99ec5bbe5991a
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2025-01-12 12:56:12 +0100

    add 2 projects

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -1,5 +1,39 @@
 export const projects = [
   {
+    "title": "SETO LOGISTIC | On-Demand Transport Solutions",
+    "type": "Single project (Full Stack Developer)",
+    "description":
+      "Developed a logistics web application providing users with tools to calculate transportation routes, select vehicles, and manage orders. Implemented key features such as dynamic pricing based on distance and vehicle type, multilingual support, and payment integration. Enhanced user experience with responsive design, interactive elements, and accessibility optimizations. The project utilized React for the frontend and integrated Netlify Functions for backend operations. Achievement: Successfully built a scalable, multilingual order system with secure payment functionality and real-time order tracking.",
+    "technologies": [
+      "React",
+      "TypeScript",
+      "Styled-Components",
+      "i18next",
+      "React Router",
+      "Toastify",
+      "Netlify Functions"
+    ],
+    "projectLink": "https://logistic-sito.netlify.app",
+    "codeLink": "https://github.com/Svitly4nyi-Vla2yslav/logistic-sito"
+  },
+  {
+    "title": "Prix de l'or | Reliable Information for Precious Metal Evaluation",
+    "type": "Single project (Full Stack Developer)",
+    "description":
+      "Developed a web platform designed to provide users with comprehensive information about gold and other precious metals. The site includes features such as detailed descriptions of gold types (karats, coins, etc.), partner organizations offering metal evaluation services, and multilingual support to cater to a diverse audience. The project focused on delivering a responsive and user-friendly design, integrated with Netlify Functions for seamless data management. Achievement: Successfully created an informative and visually appealing platform tailored to the French market, ensuring accessibility and usability.",
+    "technologies": [
+      "React",
+      "TypeScript",
+      "Styled-Components",
+      "React Router",
+      "FormSpree",
+      "AOS"
+    ],
+    "projectLink": "https://change-cold.netlify.app/",
+    "codeLink": "https://github.com/Svitly4nyi-Vla2yslav/best-gold"
+  },
+
+  {
     title: "Angel Systems | Innovation Conveyor",
     type: "Single project (Full Stack Developer)",
     description:
@@ -15,60 +49,60 @@ export const projects = [
     projectLink: "https://www.angelsystems.ch",
     codeLink: "https://github.com/Svitly4nyi-Vla2yslav/healthy-sitting",
   },
-    {
-      title: "Tasty Treats",
-      type: "Team project (Developer).",
-      description:
-        "Page with recipe search with theme toggle, various search filters, pagination, modal windows, with dynamic content rendering, animations and adding recipe to favorites logic. My tasks: search filters, in catalog, on-scroll animations.",
-      technologies: [
-        "HTML",
-        "SCSS",
-        "JavaScript",
-        "REST API",
-        "Responsive Layout",
-      ],
-      projectLink: "https://andreys86.github.io/Project-Yummy-Creators-/",
-      codeLink: "https://github.com/AndreyS86/Project-Yummy-Creators-",
-    },
-    {
-      title: "ContactHub",
-      type: "Training project.",
-      description:
-        "A contact storage app with authorization, asynchronous Redux and a a beautiful interface using Ant-design.",
-      technologies: [
-        "React",
-        "React-Router",
-        "Redux tookit",
-        "Styled Components",
-        "Ant-design",
-        "REST API",
-        "axios",
-        "react-particles",
-      ],
-      projectLink: "https://svitly4nyi-vla2yslav.github.io/goit-react-hw-08-phonebook/",
-      codeLink: "https://github.com/Svitly4nyi-Vla2yslav/goit-react-hw-08-phonebook/tree/main",
-    },
-    {
-      title: "The Watch Spot",
-      type: "Team project.",
-      description:
-        "A landing page with wide functionality, including theme switch, selecting watch from catalog, slider, mobile menu, and modal window, scroll-up button, and obviously, beautiful animations. My tasks: modal window, mobile menu.",
-      technologies: ["HTML/CSS", "JavaScript", "Responsive Layout"],
-      projectLink: "https://startonio.github.io/project-05/",
-      codeLink: "https://github.com/Startonio/project-05",
-    },
-    {
-      title: "Filmic explorer",
-      type: "Training project.",
-      description: "A small app about movies to improve routing.",
-      technologies: [
-        "React",
-        "React-Router",
-        "Styled Components",
-        "REST API",
-        "axios",
-      ],
-      projectLink: "https://svitly4nyi-vla2yslav.github.io/goit-react-hw-05-movies/",
-      codeLink: "https://github.com/Svitly4nyi-Vla2yslav/goit-react-hw-05-movies",
-    },
-  ];
+  {
+    title: "Tasty Treats",
+    type: "Team project (Developer).",
+    description:
+      "Page with recipe search with theme toggle, various search filters, pagination, modal windows, with dynamic content rendering, animations and adding recipe to favorites logic. My tasks: search filters, in catalog, on-scroll animations.",
+    technologies: [
+      "HTML",
+      "SCSS",
+      "JavaScript",
+      "REST API",
+      "Responsive Layout",
+    ],
+    projectLink: "https://andreys86.github.io/Project-Yummy-Creators-/",
+    codeLink: "https://github.com/AndreyS86/Project-Yummy-Creators-",
+  },
+  {
+    title: "ContactHub",
+    type: "Training project.",
+    description:
+      "A contact storage app with authorization, asynchronous Redux and a a beautiful interface using Ant-design.",
+    technologies: [
+      "React",
+      "React-Router",
+      "Redux tookit",
+      "Styled Components",
+      "Ant-design",
+      "REST API",
+      "axios",
+      "react-particles",
+    ],
+    projectLink: "https://svitly4nyi-vla2yslav.github.io/goit-react-hw-08-phonebook/",
+    codeLink: "https://github.com/Svitly4nyi-Vla2yslav/goit-react-hw-08-phonebook/tree/main",
+  },
+  {
+    title: "The Watch Spot",
+    type: "Team project.",
+    description:
+      "A landing page with wide functionality, including theme switch, selecting watch from catalog, slider, mobile menu, and modal window, scroll-up button, and obviously, beautiful animations. My tasks: modal window, mobile menu.",
+    technologies: ["HTML/CSS", "JavaScript", "Responsive Layout"],
+    projectLink: "https://startonio.github.io/project-05/",
+    codeLink: "https://github.com/Startonio/project-05",
+  },
+  {
+    title: "Filmic explorer",
+    type: "Training project.",
+    description: "A small app about movies to improve routing.",
+    technologies: [
+      "React",
+      "React-Router",
+      "Styled Components",
+      "REST API",
+      "axios",
+    ],
+    projectLink: "https://svitly4nyi-vla2yslav.github.io/goit-react-hw-05-movies/",
+    codeLink: "https://github.com/Svitly4nyi-Vla2yslav/goit-react-hw-05-movies",
+  },
+];
